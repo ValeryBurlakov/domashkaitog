e4cb739275fe18a08867b47910ee1f9737ced526
Valery <wogl@mail.ru>
2022-12-25 01:49:43 +0300

    stage 4. Code

--- zadacha/Program.cs
+++ zadacha/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Здравствуйте. Будем выводить элементы длина которых меньше 4 символов");
+Console.WriteLine("введите элементы массива через пробел: ");
+
+string? unit = Console.ReadLine();
+string[] elements = unit!.Split(" ");
+
+Console.WriteLine("элементы, где меньше 4 символов:");
+int count = 0;
+for (int i = 0; i < elements.Length; i++)
+{
+    if (elements[i].Length <= 3)
+    {
+        count++;
+    }
+}
+
+string[] result = new string[count];
+int t = 0;
+
+for (int i = 0; i < elements.Length; i++)
+{
+    if (elements[i].Length <= 3)
+    {
+        result[t] = elements[i];
+        t++;
+    }
+}
+
+Console.WriteLine("[" + string.Join(", ", result) + "]");
